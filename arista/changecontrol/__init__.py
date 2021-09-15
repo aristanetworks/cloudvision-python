b17cd343bbbd5db3241b686ffd297954b4c78356
Zach Marcantel <zmarcantel@arista.com>
2021-09-14 19:46:42 -0500

    resources/release: changecontrol.v1 and endpointlocation.v1
    Change-Id: I957747fedca7fa2e1bc3fa9cef39558a21d78b54

---
+++ arista/changecontrol/__init__.py
@@ -0,0 +1,1 @@
+# Copyright (c) 2021 Arista Networks, Inc.  All rights reserved.
---
+++ arista/endpointlocation/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2021 Arista Networks, Inc.  All rights reserved.
+
+from arista.endpointlocation.v1 import endpointlocation_pb2 as models
+import arista.endpointlocation.v1.services
