79a69aa9141f9f7fb8941036afa0270d413c9080
Arista Jenkins <jenkins@arista.com>
2022-03-30 18:49:47 +0000

    release: release dashboard.v1 and syslog.v1
    Change-Id: I9e54c0e8e13de8559ab9521727849dca53200643

---
+++ arista/dashboard/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
+
+from arista.dashboard.v1 import dashboard_pb2 as models
+import arista.dashboard.v1.services
--- fmp/__init__.py
+++ fmp/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
 
+import wrappers_pb2 as wrappers
 import inet_pb2 as inet
 import yang_pb2 as yang
-import wrappers_pb2 as wrappers
 import extensions_pb2 as extensions
