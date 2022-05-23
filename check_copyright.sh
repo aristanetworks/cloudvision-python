95fcc1f7874a95fc411e6366c03d87c39252ae4f
Cian McGrath <mcgrathc@arista.com>
2022-05-23 10:50:49 +0100

    Examples: Fix incorrect notice and add further checks for them
    Adds in the copyright check to be a part of the linting directive
    
    Adds further checks to copyright check shell script to check for
    use of Apache License instead of generic Arista Copyright
    
    Change-Id: Ib555f1bb9e2c8d306100d44c61c4697e2f1d215b

--- check_copyright.sh
+++ check_copyright.sh
@@ -5,13 +5,32 @@
 
 # egrep that comes with our Linux distro doesn't like \d, so use [0-9]
 notice='Copyright \(c\) 20[0-9][0-9] Arista Networks, Inc.'
+apacheNotice="Use of this source code is governed by the Apache License 2.0"
+apacheNoticeSource="that can be found in the COPYING file."
+confidential="Confidential and Proprietary"
+
 files=`git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r HEAD | \
 	egrep '\.(go|proto|py|sh)$' | grep -v '_pb2\.py$' | grep -v '_pb2_grpc\.py$'`
 status=0
 
 for file in $files; do
+	if egrep -q "$confidential" $file; then
+		# Need to omit the check_copyright script from this check as it will always fail
+		if [ "$file" != "check_copyright.sh" ]; then
+			echo "$file: use of confidential material per copyright notice"
+			status=1
+		fi
+	fi
 	if ! egrep -q "$notice" $file; then
-		echo "$file: missing or incorrect copyright notice"
+		echo "$file: missing or incorrect copyright notice for Arista"
+		status=1
+	fi
+	if ! egrep -q "$apacheNotice" $file; then
+		echo "$file: missing or incorrect Apache Licence 2.0 notice"
+		status=1
+	fi
+	if ! egrep -q "$apacheNoticeSource" $file; then
+		echo "$file: missing or incorrect Apache Licence 2.0 notice COPYING directive"
 		status=1
 	fi
 done
--- examples/resources/studio/intf_config.py
+++ examples/resources/studio/intf_config.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2021 Arista Networks, Inc.  All rights reserved.
-# Arista Networks, Inc. Confidential and Proprietary.
-# Subject to Arista Networks, Inc.'s EULA.
-# FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
+# Copyright (c) 2021 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
 
 import argparse
 import grpc
