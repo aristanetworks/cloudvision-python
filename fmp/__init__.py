a7fa46a4b3a0bf04012a3c82491f8954cdc2fa3a
Cian McGrath <mcgrathc@arista.com>
2022-06-30 17:18:59 +0100

    fmp: fix imports for when package is installed as module
    Currently using the fmp package in cloudvision-python repo causes
    ModuleNotFound exception, add the fmp import to fix this
    
    Change-Id: Ib7b896c6265b1cadd6eb43165f73c40b14ef74e2

--- fmp/__init__.py
+++ fmp/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
 
-import wrappers_pb2 as wrappers
-import inet_pb2 as inet
-import yang_pb2 as yang
-import extensions_pb2 as extensions
+import fmp.wrappers_pb2 as wrappers
+import fmp.inet_pb2 as inet
+import fmp.yang_pb2 as yang
+import fmp.extensions_pb2 as extensions
