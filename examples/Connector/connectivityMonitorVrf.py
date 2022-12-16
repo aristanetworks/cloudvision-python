d24624843ec3510ceaa01bc696de1fe1373bb4fd
Tamas Plugor <tamas@arista.com>
2022-12-16 18:34:51 +0000

    examples: refactor Connectivty monitor scripts to use getAll with wildcards
    Change-Id: Ie1b73eb7653d86b37a3e8c9c17076b5036baa4b8

--- examples/Connector/connectivityMonitorVrf.py
+++ examples/Connector/connectivityMonitorVrf.py
@@ -1,22 +1,51 @@
-# Copyright (c) 2021 Arista Networks, Inc.
+# Copyright (c) 2022 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the COPYING file.
 
 from cloudvision.Connector.grpc_client import GRPCClient, create_query
 from cloudvision.Connector.codec import Wildcard, Path
+from cloudvision.Connector.codec.custom_types import FrozenDict
 from utils import pretty_print
 from parser import base
-import json
 
 debug = False
 
 
-def get(client, dataset, pathElts):
-    ''' Returns a query on a path element'''
-    result = {}
-    query = [
-        create_query([(pathElts, [])], dataset)
+def getConnMon(client, device=None):
+    """Returns a query on a path element"""
+    device = device if device else Wildcard()
+    pathElts = [
+        "Devices",
+        device,
+        "versioned-data",
+        "connectivityMonitorVrf",
+        "stats",
+        Wildcard(),
+        Wildcard(),
     ]
+    dataset = "analytics"
+    result = {}
+    query = [create_query([(pathElts, [])], dataset)]
+
+    for batch in client.get(query):
+        for notif in batch["notifications"]:
+            # there are some static path pointers at this path so we remove them from the update results
+            notif["updates"].pop("aggregate", None)
+            notif["updates"].pop("anomaly", None)
+            # if the updates only contained the static path pointers then continue as there is no data of interest here
+            if not notif["updates"]:
+                continue
+            pathElts = notif["path_elements"]
+            connMonKey = (pathElts[1], pathElts[5], pathElts[6])
+            result[connMonKey] = notif["updates"]
+
+    return result
+
+
+def get(client, dataset, pathElts):
+    """Returns a query on a path element"""
+    result = {}
+    query = [create_query([(pathElts, [])], dataset)]
 
     for batch in client.get(query):
         for notif in batch["notifications"]:
@@ -26,117 +55,89 @@ def get(client, dataset, pathElts):
     return result
 
 
-def getConMonKeys(client, dId):
+def getConnMonCfg(client, device=None):
+    device = device if device else Wildcard()
     pathElts = [
         "Devices",
-        dId,
-        "versioned-data",
-        "connectivityMonitorVrf",
-        "stats"
-    ]
-    dataset = "analytics"
-    return get(client, dataset, pathElts)
-
-
-def getConnMon(client, dId, key):
-    pathElts = [
-        "Devices",
-        dId,
-        "versioned-data",
-        "connectivityMonitorVrf",
-        "stats",
-        key,
-    ]
-    dataset = "analytics"
-    return get(client, dataset, pathElts)
-
-
-def getConnMonCfg(client, dId, key):
-    pathElts = [
-        "Devices",
-        dId,
+        device,
         "versioned-data",
         "connectivityMonitorVrf",
         "config",
-        key
+        Wildcard(),
     ]
     dataset = "analytics"
-    return get(client, dataset, pathElts)
+    result = {}
+    query = [create_query([(pathElts, [])], dataset)]
 
-
-def getAllConnMon(client, dId, key, interface):
-    pathElts = [
-        "Devices",
-        dId,
-        "versioned-data",
-        "connectivityMonitorVrf",
-        "stats",
-        key,
-        interface
-    ]
-    dataset = "analytics"
-    return get(client, dataset, pathElts)
+    for batch in client.get(query):
+        for notif in batch["notifications"]:
+            if not notif["updates"]:
+                continue
+            pathElts = notif["path_elements"]
+            connMonKey = (pathElts[1], pathElts[5])
+            result[connMonKey] = notif["updates"]
+    return result
 
 
 def getSwitchesInfo(client):
-    pathElts = [
-        "DatasetInfo",
-        "Devices"
-    ]
+    pathElts = ["DatasetInfo", "Devices"]
     dataset = "analytics"
     return get(client, dataset, pathElts)
 
 
-def report(client, device):
-    keys = getConMonKeys(client, device)
-    key_list = list(keys.keys())
+def report(client, data, configData):
     datasetInfo = getSwitchesInfo(client)
-    hostname = datasetInfo[device]['hostname']
-
-    for i in key_list:
-        vrf = i['vrfName']
-        host = i['hostName']
-        stats = getConnMon(client, device, i)
-        interfaces = list(stats.keys())
-        for intf in interfaces:
-            stats_intf = getAllConnMon(client, device, i, intf)
-            httpResp = stats_intf['httpResponseTime']
-            jitter = stats_intf['jitter']
-            latency = stats_intf['latency']
-            pktloss = stats_intf['packetLoss']
-            ipaddr = getConnMonCfg(client, device, i)['ipAddr']
-            hdr_part1 = f"{hostname + ' (' + vrf + '/' + intf + ') to ' + host:<50}"
-            hdr_part2 = f"{ipaddr:<30}{str(httpResp) + 'ms':<30}{str(jitter) + 'ms':<30}"
-            hdr_part3 = f"{str(latency) + 'ms':<30}{str(pktloss)  + '%':<30}"
-            print(hdr_part1 + hdr_part2 + hdr_part3)
+    for k, v in data.items():
+        hostname = datasetInfo[k[0]]["hostname"]
+        vrf = k[1]["vrfName"]
+        httpResp = v["httpResponseTime"]
+        jitter = v["jitter"]
+        latency = v["latency"]
+        pktloss = v["packetLoss"]
+        host = k[1]["hostName"]
+        ipaddr = configData[(k[0], k[1])]["ipAddr"]
+        intf = k[2]
+        hdr_part1 = f"{hostname + ' (' + vrf + '/' + intf + ') to ' + host:<50}"
+        hdr_part2 = f"{ipaddr:<30}{str(httpResp) + 'ms':<30}{str(jitter) + 'ms':<30}"
+        hdr_part3 = f"{str(latency) + 'ms':<30}{str(pktloss)  + '%':<30}"
+        print(hdr_part1 + hdr_part2 + hdr_part3)
 
 
 def main(apiserverAddr, token=None, certs=None, key=None, ca=None):
 
-    with GRPCClient(apiserverAddr, token=token, key=key,
-                    ca=ca, certs=certs) as client:
-        datasetInfo = getSwitchesInfo(client)
-        col1 = 'Connection'
-        col2 = 'Host IP Address per VRF'
-        col3 = 'HTTP RESPONSE TIME per VRF'
-        col4 = 'Jitter per VRF'
-        col5 = 'Latency per VRF'
-        col6 = 'Packet Loss Per VRF'
-        dashboardColumns = f"{col1:<50}{col2:<30}{col3:<30}{col4:<30}{col5:<30}{col6:<30}"
+    with GRPCClient(apiserverAddr, token=token, key=key, ca=ca, certs=certs) as client:
+        col1 = "Connection"
+        col2 = "Host IP Address per VRF"
+        col3 = "HTTP RESPONSE TIME per VRF"
+        col4 = "Jitter per VRF"
+        col5 = "Latency per VRF"
+        col6 = "Packet Loss Per VRF"
+        dashboardColumns = (
+            f"{col1:<50}{col2:<30}{col3:<30}{col4:<30}{col5:<30}{col6:<30}"
+        )
         if args.device:
-            print(dashboardColumns)
-            report(client, args.device)
+            device = args.device
         else:
-            # Detect which devices are running Connectivity Monitor
-            print(dashboardColumns)
-            for elem in datasetInfo.keys():
-                if not (getConMonKeys(client, elem) == {}):
-                    report(client, elem)
+            device = None
+        data = getConnMon(client, device)
+        configData = getConnMonCfg(client, device)
+        print(dashboardColumns)
+        report(client, data, configData)
+
     return 0
 
 
 if __name__ == "__main__":
-    base.add_argument("--device", type=str, help="device to subscribe to")
+    base.add_argument(
+        "--device", type=str, help="device (by SerialNumber) to subscribe to"
+    )
     args = base.parse_args()
-    exit(main(args.apiserver, certs=args.certFile, key=args.keyFile,
-              ca=args.caFile, token=args.tokenFile))
+    exit(
+        main(
+            args.apiserver,
+            certs=args.certFile,
+            key=args.keyFile,
+            ca=args.caFile,
+            token=args.tokenFile,
+        )
+    )
