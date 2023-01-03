26f7de64397555924b3b893ec6139f131d7007a6
Toran Sahu <toran@arista.com>
2023-01-03 18:07:54 +0530

    Connector: Add auth/cert.py for CSR, Key, & Cert handling
    Fixes: BUG762118
    Change-Id: Ib1fd2585257aaed1b8af9424829133fc10a9a919

---
+++ cloudvision/Connector/auth/cert.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+from typing import Any, Optional, Tuple
+
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+
+
+def gen_csr_der(cert_path: str, key_path: str) -> bytes:
+    """Generate ASN.1 DER encoded CSR.
+
+    :param cert_path: Certificate file path (PEM encoded)
+    :param key_path: Private Key file path (PEM encoded)
+    :return: ASN.1 DER encoded CSR data
+    """
+
+    cert, key = load_key_cert_pair(cert_path, key_path)
+    csr = create_csr(cert, key)
+    return csr.public_bytes(serialization.Encoding.DER)
+
+
+def create_csr(cert: x509.Certificate, key: Any) -> x509.CertificateSigningRequest:
+    """Create CSR from given cert and private key.
+
+    :param cert: Certificate object
+    :param key: Private Key object
+    :return: CSR object
+    """
+
+    return (
+        x509.CertificateSigningRequestBuilder().subject_name(cert.subject)
+        # NOTE: Stick to the same old signature hash algo used earlier
+        .sign(key, cert.signature_hash_algorithm)
+    )
+
+
+def load_key_cert_pair(cert_path: str, key_path: str) -> Tuple[x509.Certificate, Any]:
+    """Load key & cert files.
+
+    :param cert_path: Certificate file path (PEM encoded)
+    :param key_path: Private key file path (PEM encoded)
+    :return: Certificate and Key object
+    """
+
+    return load_cert(cert_path), load_key(key_path)
+
+
+def load_cert(cert_path: str) -> x509.Certificate:
+    """Load certificate file (PEM encoded).
+
+    :param cert_path: Certificate file (PEM encoded)
+    :return: Certificate object
+    """
+
+    cert_pem: bytes
+    with open(cert_path, "rb") as f:
+        cert_pem = f.read()
+    return cert_from_pem(cert_pem)
+
+
+def cert_from_pem(cert_pem: bytes) -> x509.Certificate:
+    """Generate cert object from PEM encoded cert.
+
+    :param cert_pem: Certificate PEM data
+    :return: Certificate object
+    """
+
+    return x509.load_pem_x509_certificate(cert_pem)
+
+
+def load_key(key_path: str, passphrase: str = None) -> Any:
+    """Load private key file (PEM encoded).
+
+    :param key_path: Private key file (PEM encoded)
+    :param passphrase: Passphrase used to decrypt the key
+    :return: Private key object
+    """
+
+    key_pem: bytes
+    with open(key_path, "rb") as f:
+        key_pem = f.read()
+
+    _passphrase: Optional[bytes] = bytes(passphrase, "utf-8") if passphrase else None
+    return key_from_pem(key_pem, _passphrase)
+
+
+def key_from_pem(key_pem: bytes, passphrase: bytes = None) -> Any:
+    """Generate key object from PEM encoded private key.
+
+    :param key_pem: Private key (PEM encoded)
+    :param passphrase: Passphrase used to decrypt the key
+    :return: Private key object
+    """
+
+    return serialization.load_pem_private_key(key_pem, password=passphrase)
---
+++ test/connector/auth/test_cert.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+"""Test certificate helper functions."""
+
+
+from pathlib import Path
+from typing import Union
+
+import pytest
+from cryptography import x509
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.x509.base import load_der_x509_csr
+
+from cloudvision.Connector.auth.cert import (
+    cert_from_pem,
+    create_csr,
+    gen_csr_der,
+    key_from_pem,
+    load_cert,
+    load_key,
+    load_key_cert_pair,
+)
+
+THIS_DIR = Path(__file__).parent
+TEST_DIR = THIS_DIR.parent.parent
+TEST_DATA_DIR = Path.joinpath(TEST_DIR, "test_data")
+
+
+def fqfp(file_name: str) -> Path:
+    """Fully Qualified File Path.
+
+    :param file_name: Test file name
+    :return: Fully qualified/absolute file path of given test file
+    """
+
+    return Path.joinpath(TEST_DATA_DIR, file_name)
+
+
+def data(file_name: str, mode: str = "rb") -> Union[str, bytes]:
+    """Data of given test file.
+
+    :param file_name: Test file name
+    :param mode: Mode to read test file
+    :return: Data of the given file
+    """
+
+    with open(fqfp(file_name), mode) as f:
+        return f.read()
+
+
+class TestCert:
+    @pytest.mark.parametrize("cert_path, key_path", [(fqfp("cert.pem"), fqfp("key.pem"))])
+    def test_gen_csr_der(self, cert_path, key_path):
+        csr_der = gen_csr_der(str(cert_path), str(key_path))
+        csr = load_der_x509_csr(csr_der)
+        cert, key = load_key_cert_pair(cert_path, key_path)
+        assert csr.subject == cert.subject
+        assert csr.signature_hash_algorithm == cert.signature_hash_algorithm
+        assert csr.public_key().public_bytes(
+            Encoding.DER, PublicFormat.SubjectPublicKeyInfo
+        ) == key.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+        assert csr.public_key().public_bytes(
+            Encoding.DER, PublicFormat.SubjectPublicKeyInfo
+        ) == cert.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+
+    @pytest.mark.parametrize("cert_path, key_path", [(fqfp("cert.pem"), fqfp("key.pem"))])
+    def test_create_csr(self, cert_path, key_path):
+        cert, key = load_key_cert_pair(cert_path, key_path)
+        csr: x509.CertificateSigningRequest = create_csr(cert, key)
+        assert csr.subject == cert.subject
+        assert csr.signature_hash_algorithm == cert.signature_hash_algorithm
+        assert csr.public_key().public_bytes(
+            Encoding.DER, PublicFormat.SubjectPublicKeyInfo
+        ) == key.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+        assert csr.public_key().public_bytes(
+            Encoding.DER, PublicFormat.SubjectPublicKeyInfo
+        ) == cert.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+
+    @pytest.mark.parametrize("cert_path, key_path", [(fqfp("cert.pem"), fqfp("key.pem"))])
+    def test_load_key_cert_pair(self, cert_path: str, key_path: str):
+        cert, key = load_key_cert_pair(cert_path, key_path)
+        assert cert is not None
+        assert key is not None
+        assert isinstance(cert, x509.Certificate)
+        assert isinstance(key, rsa.RSAPrivateKey)
+        assert key.public_key().public_bytes(
+            Encoding.DER, PublicFormat.SubjectPublicKeyInfo
+        ) == cert.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+
+    @pytest.mark.parametrize("cert_path", [fqfp("cert.pem")])
+    def test_load_cert(self, cert_path: str):
+        cert = load_cert(cert_path)
+        assert cert is not None
+        assert isinstance(cert, x509.Certificate)
+
+    @pytest.mark.parametrize("key_path", [fqfp("key.pem")])
+    def test_load_key(self, key_path: str):
+        key = load_key(key_path)
+        assert key is not None
+        assert isinstance(key, rsa.RSAPrivateKey)
+
+    @pytest.mark.parametrize("cert_pem", [data("cert.pem")])
+    def test_cert_from_pem(self, cert_pem):
+        cert = cert_from_pem(cert_pem)
+        assert cert is not None
+        assert isinstance(cert, x509.Certificate)
+
+    @pytest.mark.parametrize("key_pem", [data("key.pem")])
+    def test_key_from_pem(self, key_pem):
+        key = key_from_pem(key_pem)
+        assert key is not None
+        assert isinstance(key, rsa.RSAPrivateKey)
