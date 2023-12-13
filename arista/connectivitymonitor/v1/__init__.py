fc073017e1f15d1fa1443c13510eb160bff6fdc8
Arista Jenkins <jenkins@arista.com>
2023-12-13 15:22:27 +0000

    release/arista: Release connectivitymonitor
    Releases connectivitymonitor.v1
    
    [GENERATED_COMMIT]
    
    Change-Id: Ic41ccc65b868bc84e04adc252d58a6a1ba2d65fb

---
+++ arista/connectivitymonitor/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.
+
+from arista.connectivitymonitor.v1 import connectivitymonitor_pb2 as models
+import arista.connectivitymonitor.v1.services
