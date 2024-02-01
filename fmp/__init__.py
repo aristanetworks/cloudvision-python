517deca810b2667b9cbdb89493465f3103c1a3b4
Arista Jenkins <jenkins@arista.com>
2024-02-01 04:55:00 +0000

    update/event.v1: add DEBUG severity
    XXX_SKIP_CHECK_BREAKING
    
    [GENERATED_COMMIT]
    
    Change-Id: Iaa8793b615cd5b022f365b8b3da36cf1965f30ca

--- fmp/__init__.py
+++ fmp/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 Arista Networks, Inc.  All rights reserved.
 
-import fmp.wrappers_pb2 as wrappers
-import fmp.extensions_pb2 as extensions
-import fmp.deletes_pb2 as deletes
 import fmp.inet_pb2 as inet
+import fmp.extensions_pb2 as extensions
 import fmp.yang_pb2 as yang
+import fmp.deletes_pb2 as deletes
+import fmp.wrappers_pb2 as wrappers
