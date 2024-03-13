b33bb217d25b12f6554106a6dcf716ec886fd6f6
Arista Jenkins <jenkins@arista.com>
2024-03-13 20:43:35 +0000

    fmp/pages: touch pages proto
    touch pages.proto to trigger downstream gerrit changes.
    
    [GENERATED_COMMIT]
    
    Change-Id: I6acc73d4a198e5234921e340b249a932340076e8

--- fmp/__init__.py
+++ fmp/__init__.py
@@ -2,6 +2,7 @@
 
 import fmp.wrappers_pb2 as wrappers
 import fmp.extensions_pb2 as extensions
+import fmp.pages_pb2 as pages
 import fmp.deletes_pb2 as deletes
 import fmp.inet_pb2 as inet
 import fmp.yang_pb2 as yang
