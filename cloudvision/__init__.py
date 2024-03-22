970b471474f9ae1b726aa45e3714c9a08fece4aa
Cian McGrath <mcgrathc@arista.com>
2024-03-22 15:59:23 +0000

    repo: convert to using toml file for installation
    Change-Id: I0a64de54ed591e9a5ca7089822267cb0988a9181

--- cloudvision/__init__.py
+++ cloudvision/__init__.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the COPYING file.
 
+import importlib.metadata
+
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore  # mypy issue #1422
 
-__version__ = "1.19.0"
+__version__ = importlib.metadata.version("cloudvision")
