8c0c93fa4c4d2094b2c31a06010c107bff6a2248
Arista Jenkins <jenkins@arista.com>
2024-08-01 18:51:22 +0000

    imagestatus.v1: Add an error code ERROR_CODE_ARCH_UNSUPPORTED
    Add a new error code ERROR_CODE_ARCH_UNSUPPORTED.
    
    ERROR_CODE_ARCH_UNSUPPORTED represents the case where the given image
    architecture is not supported by CloudVision.
    
    [GENERATED_COMMIT]
    
    Change-Id: I0d0aaea341936e0ee443a9a9d11910ea0f873239

--- fmp/__init__.py
+++ fmp/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2024 Arista Networks, Inc.  All rights reserved.
 
-import fmp.wrappers_pb2 as wrappers
-import fmp.extensions_pb2 as extensions
 import fmp.pages_pb2 as pages
-import fmp.deletes_pb2 as deletes
 import fmp.inet_pb2 as inet
+import fmp.extensions_pb2 as extensions
 import fmp.yang_pb2 as yang
+import fmp.deletes_pb2 as deletes
+import fmp.wrappers_pb2 as wrappers
