652f980330193bcf0731c08cbe15c781b3f99b1a
Arista Jenkins <jenkins@arista.com>
2025-01-06 10:08:43 +0000

    update/changecontrol.v1: Add Step Info within CC Stage
    Updated the changecontrol.v1 service proto with the definitions
    of the step statuses for actions performed by CC :
    - message StepInfo : holds the status information for the action step
    - message ActionSteps : holds a mapping between step sequence number and its StepInfo
    - message Stage : holds a new field representing the ActionSteps for the stage.
    
    [GENERATED_COMMIT]
    
    Change-Id: I5525c9ff5dd1d7cfb61bb559680430b6c63e15d9

--- arista/endpointlocation/v1/__init__.py
+++ arista/endpointlocation/v1/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Arista Networks, Inc.  All rights reserved.
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
 
 from arista.endpointlocation.v1 import endpointlocation_pb2 as models
 import arista.endpointlocation.v1.services
