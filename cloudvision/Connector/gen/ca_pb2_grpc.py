bfbe97849fffabad138afe7c47f3821b7c4633b6
Cian McGrath <mcgrathc@arista.com>
2025-01-29 10:52:21 +0000

    cloudvision: Downgrade connector stubs to be generated with grpc 1.67.1
    Issue has been noticed due to https://github.com/grpc/grpc/issues/38290
    grpc 1.67.1 does not have this issue, downgrade stubs to be compatible
    
    Change-Id: I2183cfbcd85f4723bdeb7fb5c8f960bb392e3060

--- cloudvision/Connector/gen/ca_pb2_grpc.py
+++ cloudvision/Connector/gen/ca_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import ca_pb2 as ca__pb2
 
-GRPC_GENERATED_VERSION = '1.68.1'
+GRPC_GENERATED_VERSION = '1.67.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
--- cloudvision/Connector/gen/router_pb2_grpc.py
+++ cloudvision/Connector/gen/router_pb2_grpc.py
@@ -7,7 +7,7 @@ from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 import notification_pb2 as notification__pb2
 import router_pb2 as router__pb2
 
-GRPC_GENERATED_VERSION = '1.68.1'
+GRPC_GENERATED_VERSION = '1.67.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
