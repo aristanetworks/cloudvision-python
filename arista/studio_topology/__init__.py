a9968746a787804c5ea07b01b5231c3dac88c009
Arista Jenkins <jenkins@arista.com>
2025-02-06 22:45:45 +0000

    arista/studio_topology.v1: release studio_topology.proto
    - studio_topology.proto is ready to be published
    
    [GENERATED_COMMIT]
    
    Change-Id: Idbfad4518e804ed1ae8dfd94af6deefe76d95d7a

---
+++ arista/studio_topology/__init__.py
@@ -0,0 +1,1 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
---
+++ arista/studio_topology/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+
+from arista.studio_topology.v1 import studio_topology_pb2 as models
+import arista.studio_topology.v1.services
