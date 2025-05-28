724a90cda0ea07174e356444a03c2105889a3d73
Arista Jenkins <jenkins@arista.com>
2025-05-28 11:49:22 +0000

    release/action.v1: publishing new API
    [GENERATED_COMMIT]
    
    Change-Id: I2971a977f57835d508a404bfa49a6ff3c28fa0c5

---
+++ arista/action/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+
+from arista.action.v1 import action_pb2 as models
+import arista.action.v1.services
---
+++ arista/action/v1/services/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+
+# pylint: disable=wildcard-import
+from arista.action.v1.services.gen_pb2 import *
+# pylint: disable=wildcard-import
+from arista.action.v1.services.gen_pb2_grpc import *
