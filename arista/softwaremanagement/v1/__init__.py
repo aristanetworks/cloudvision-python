9dbd8dbf599eb88ef0a78963579d025a6776fe43
Arista Jenkins <jenkins@arista.com>
2025-05-30 19:42:10 +0000

    release/softwaremanagement.v1: Releasing software management proto
    - Release softwaremanagement.v1 proto to the world
    
    [GENERATED_COMMIT]
    
    Change-Id: I6e4ebbe5f6a8d8d4a0994857d2c9048ea854f6df

---
+++ arista/softwaremanagement/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+
+from arista.softwaremanagement.v1 import softwaremanagement_pb2 as models
+import arista.softwaremanagement.v1.services
