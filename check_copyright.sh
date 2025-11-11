cbb79706ad310e982a56d3e324f55ef1e9dc1324
Roman Statkevych <rstatkevych@arista.com>
2025-11-11 17:15:48 +0000

    check_copyright: Fix dubious ownership and script logic
    In jenkins, ./check_copyright.sh was failing due to 'dubious repo
    ownership'.
    Additionally, when `make lint` is run, `git diff-tree HEAD` doesn't show
    anything, because it only compares non-staged changes. Correct way is
    to compare files with the base revision (this would almost always be
    trunk) by getting its hash from `git log --first-parent` instead
    
    Change-Id: I78b843c2142ce7edf6c3d1b2070dd3acbcbdfcf9

--- check_copyright.sh
+++ check_copyright.sh
@@ -3,6 +3,14 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the COPYING file.
 
+# This script checks if all the changed files in current revision
+# have proper copyright headers
+#
+# Usage: ./check_copyright [<branch>]
+#
+# 	branch is optional - you can provide base branch manually (e.g `trunk`)
+#	otherwise it would use `git log --first-parent`
+
 # egrep that comes with our Linux distro doesn't like \d, so use [0-9]
 notice='Copyright \(c\) 20[0-9][0-9] Arista Networks, Inc.'
 apacheNotice='Use of this source code is governed by the Apache License 2.0'
@@ -11,7 +19,22 @@ confidential='Confidential and Proprietary'
 generatedCodePaths='(^arista|^fmp).*'
 copyrightCheck='.*(check_copyright.sh)'
 
-files=`git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r HEAD | \
+# To omit dubious ownership git errors
+repoFolder=`dirname "${BASH_SOURCE[0]}"`
+cd $repoFolder && git config --global --add safe.directory $repoFolder
+
+if [ ! -z "$1" ]; then
+	branchBaseCommit=`git show $1 --format=format:%H --no-patch`
+	exitCode=$?
+	if [ $exitCode != "0" ]; then
+		echo Error finding revision $1
+		exit $exitCode
+	fi
+else
+	branchBaseCommit=`git show --first-parent --format=format:%H --no-patch`
+fi
+
+files=`git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r $branchBaseCommit | \
 	egrep '\.(go|proto|py|sh)$' | \
 	grep -v '_pb2\.py$' | \
 	grep -v '_pb2_grpc\.py$' |\
