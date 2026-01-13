223d8e0631e83aeb213268936efaf230db05ca7a
Arista Jenkins <jenkins@arista.com>
2026-01-13 16:55:40 +0000

    release/arista_portal.v1: Releasing AristaPortal API Proto
    [GENERATED_COMMIT]
    
    Change-Id: I1fb6e3a566da8ab4823eba127411a205039e12ab

---
+++ arista/arista_portal/__init__.py
@@ -0,0 +1,1 @@
+# Copyright (c) 2026 Arista Networks, Inc.  All rights reserved.
---
+++ arista/arista_portal/v1/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2026 Arista Networks, Inc.  All rights reserved.
+
+from arista.arista_portal.v1 import arista_portal_pb2 as models
+import arista.arista_portal.v1.services
