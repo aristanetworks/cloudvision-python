c21d3b7728e8d8f934379d3d53203232301c1823
Cian McGrath <mcgrathc@arista.com>
2026-09-04 14:19:24 +0100

    examples: Fix headers in some example files
    Issue with shallow checkout in jenkins caused
    check to always pass
    
    Change-Id: I41c699cc2dfe6a2b08f615fd454335722fd00dca

--- examples/resources/studio/studio_onboarding.py
+++ examples/resources/studio/studio_onboarding.py
@@ -7,8 +7,9 @@
 # ]
 # ///
 
-# Copyright (c) 2026 Arista Networks, Inc.  All rights reserved.
-# Arista Networks, Inc. Confidential and Proprietary.
+# Copyright (c) 2026 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
 #
 # example usages:
 #   python3 studio_onboarding.py
--- examples/resources/studio/studio_static_config_simple.py
+++ examples/resources/studio/studio_static_config_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2026 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
 import argparse
 import asyncio
 import json
